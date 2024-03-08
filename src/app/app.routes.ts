b563e8e506efa328d756600d66782bdaed3ad25d
anthony-kigotho <anthonykigotho7@gmail.com>
2024-03-08 14:46:54 +0300

    basic routing from one component to another

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: 'first-component', component: FirstComponent },
+    { path: 'second-component', component: SecondComponent}
+];
